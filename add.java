c9b30048e14fc025a379e2fabd9d72e28ab3a3dc
TelukalaPreethi <preethi.telukala555@gmail.com>
2018-10-10 11:33:33 +0530

    function named add() is added.

---
+++ add.java
@@ -0,0 +1,12 @@
+class Add{
+	int add(int a,int b) {
+		b=a+b;
+		return b;
+	}
+	public static void main(String args[]) {
+		Add a=new Add();
+		int x;
+		x=a.add(2,7);
+		System.out.println(x);
+	}
+}
