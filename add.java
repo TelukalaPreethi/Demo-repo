ffdc5bcb5a2cb904e233a912f89f7fee0d006d6b
TelukalaPreethi <preethi.telukala555@gmail.com>
2018-10-12 12:10:26 +0530

    add

--- add.java
+++ add.java
@@ -1,13 +1,16 @@
 class Add{
 	int add(int a,int b) {
 		b=a+b;
+		System.out.println("End of function");
 		return b;
+		
 	}
 	public static void main(String args[]) {
 		Add a=new Add();
 		int x;
 		x=a.add(2,7);
 		System.out.println(x);
-		System.out.println("End of add.java function");
+		
+		System.out.println("End of program");
 	}
 }
